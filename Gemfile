1930a95a1a1522f2f6c04314395727514ed77e69
s17w09 <s0917.smys04@gmail.com>
2024-06-27 23:40:35 +0900

    add #3 ユーザー新規登録

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+gem 'sorcery'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'staticpages/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root 'staticpages#top'
+  resources :users, only: [:new, :create]
 
   # Defines the root path route ("/")
   # root "posts#index"
