3ff70741d7313e208fb46e0cf40753d9f0984af7
s17w09 <s0917.smys04@gmail.com>
2024-07-06 13:17:45 +0900

    update #23 スワードリセットの環境変数変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,13 +75,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true # メール送信に失敗した際に、エラーを発生させる
   config.action_mailer.delivery_method = :smtp # メールをSMTPで送る
 
+  # 一旦変数に格納
+  mail = ENV['GMAIL_ADDRESS'], #Gmailアカウントのメールアドレス
+  pass = ENV['GMAIL_PASSWORD'], #Gmailで設定したアプリパスワード
   # SMTPサーバーの設定をする
   config.action_mailer.smtp_settings = {
     port: 587,
     address:"smtp.gmail.com",
     domain: 'miniita.onrender.com', #Gmailを使う場合
-    user_name: ENV['GMAIL_ADDRESS'], #Gmailアカウントのメールアドレス
-    password: ENV['GMAIL_PASSWORD'], #Gmailで設定したアプリパスワード
+    user_name: mail, #Gmailアカウントのメールアドレス
+    password: pass, #Gmailで設定したアプリパスワード
     authentication: :plain,
     enable_starttls_auto: true
   }
