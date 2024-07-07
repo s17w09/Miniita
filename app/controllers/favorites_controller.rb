2ff5a1efc363ed79cb75def80d21da35a41efb54
s17w09 <s0917.smys04@gmail.com>
2024-07-07 18:12:16 +0900

    update #79 単体のいいねボタンの非同期処理は成功

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,16 @@ class FavoritesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @favorite = current_user.favorites.new(article_id: params[:article_id], favorite_type: params[:type])
-    @favorite.save
-    redirect_to article_path(params[:article_id])
+    if @favorite.save
+    render :create
+    end
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     @favorite = current_user.favorites.find_by(article_id: params[:article_id], favorite_type: params[:type])
-    @favorite.destroy
-    redirect_to article_path(params[:article_id])
+    if @favorite.destroy!
+      render :destroy
+    end
 end
 end
