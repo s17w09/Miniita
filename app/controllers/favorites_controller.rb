19284b1febcd41630ec8e7322979daf35f33fd90
s17w09 <s0917.smys04@gmail.com>
2024-07-08 11:15:35 +0900

    fix #31 投稿へのXシェア機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,29 @@
 class FavoritesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @favorite = current_user.favorites.new(article_id: params[:article_id], favorite_type: params[:type])
+
+    if params[:type] == 'like_first'
+      @favorite = current_user.favorites.new(article_id: params[:article_id], favorite_type: :like_first)
+    elsif params[:type] == 'like_second'
+      @favorite = current_user.favorites.new(article_id: params[:article_id], favorite_type: :like_second)
+    end
+
     if @favorite.save
-    render :create
+      render :create
     end
   end
 
   def destroy
     @article = Article.find(params[:article_id])
-    @favorite = current_user.favorites.find_by(article_id: params[:article_id], favorite_type: params[:type])
-    if @favorite.destroy!
+
+    if params[:type] == 'like_first'
+      @favorite = current_user.favorites.find_by(article_id: params[:article_id], favorite_type: :like_first)
+    elsif params[:type] == 'like_second'
+      @favorite = current_user.favorites.find_by(article_id: params[:article_id], favorite_type: :like_second)
+    end
+
+    if @favorite&.destroy!
       render :destroy
     end
-end
+  end
 end
