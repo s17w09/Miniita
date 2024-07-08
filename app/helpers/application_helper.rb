53476a983f0ffcd72181ecca42bdabbf79b94325
s17w09 <s0917.smys04@gmail.com>
2024-07-08 11:42:55 +0900

    fix #88 タブ名変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def page_title(title = "")
+    base_ttitle = "Miniita"
+    if title.empty?
+      base_ttitle
+    else
+       "#{title} | #{base_ttitle}"
+    end
+  end
+
   def render_markdown(text)
     Kramdown::Document.new(text, input: 'GFM').to_html
   end
