beac29d8d329e3fa816fdee13d1dfa84d24bfd1e
s17w09 <s0917.smys04@gmail.com>
2024-07-10 10:25:45 +0900

    fix #60_コメント投稿の非同期処理

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,29 +1,13 @@
 class FavoritesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-
-    if params[:type] == 'like_first'
-      @favorite = current_user.favorites.new(article_id: params[:article_id], favorite_type: :like_first)
-    elsif params[:type] == 'like_second'
-      @favorite = current_user.favorites.new(article_id: params[:article_id], favorite_type: :like_second)
-    end
-
-    if @favorite.save
-      render :create
-    end
+    @favorite = current_user.favorites.new(article_id: params[:article_id], favorite_type: params[:type])
+    @favorite.save
   end
 
   def destroy
     @article = Article.find(params[:article_id])
-
-    if params[:type] == 'like_first'
-      @favorite = current_user.favorites.find_by(article_id: params[:article_id], favorite_type: :like_first)
-    elsif params[:type] == 'like_second'
-      @favorite = current_user.favorites.find_by(article_id: params[:article_id], favorite_type: :like_second)
-    end
-
-    if @favorite&.destroy!
-      render :destroy
-    end
+    @favorite = current_user.favorites.find_by(article_id: params[:article_id], favorite_type: params[:type])
+    @favorite.destroy!
   end
 end
