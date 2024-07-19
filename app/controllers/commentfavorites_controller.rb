bd411fba745921e427eb494bd34cea1d288342ad
s17w09 <s0917.smys04@gmail.com>
2024-07-19 15:14:11 +0900

    fix #115_コメントへのいいね機能作成、非同期化

--- app/controllers/commentfavorites_controller.rb
+++ app/controllers/commentfavorites_controller.rb
@@ -6,8 +6,8 @@ class CommentfavoritesController < ApplicationController
   end
 
   def destroy
-    @comment = Comment.find(params[:comment_id])
-    @commentfavorite = current_user.commentfavorites.find_by(comment_id: params[:comment_id])
+    @comment = Comment.find(params[:id])
+    @commentfavorite = current_user.commentfavorites.find_by(comment_id: params[:id])
     @commentfavorite.destroy
   end
 end
