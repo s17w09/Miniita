9d3652174a916d8e1b4b57982052e0124dc9a67d
s17w09 <s0917.smys04@gmail.com>
2024-07-19 16:28:17 +0900

    fix #29 GitHub認証

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,22 @@
+class OauthsController < ApplicationController
+  def oauth
+    return redirect_to root_path if logged_in?
+    login_at(params[:provider])
+  end
+
+  def callback
+    provider = params[:provider]
+    if @user = login_from(provider)
+      redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
+    else
+      begin
+        @user = create_from(provider)
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
+      rescue
+        redirect_to root_path, error: "#{provider.titleize}アカウントでのログインに失敗しました。"
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'oauths/oauth'
+  get 'oauths/callback'
   # letter openerの設定、http://localhost:3000/letter_opener/が見れるようになる
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -8,6 +10,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root 'staticpages#top'
+  post "oauth/callback", to: "oauths#callback"
+  get "oauth/callback", to: "oauths#callback"
+  get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
+  
   resources :users, only: %i[new create] do
     collection do
       get :my_articles
