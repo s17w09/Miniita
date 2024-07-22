e8d7bc4a6b96edb9768f002db1b234611fdac990
s17w09 <s0917.smys04@gmail.com>
2024-07-22 14:43:12 +0900

    update GitHub認証後のリダイレクト先の変更

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -2,30 +2,20 @@ class OauthsController < ApplicationController
   skip_before_action :require_login, raise: false
 
   def oauth
-    return redirect_to root_path, notice: 'ログイン' if logged_in?
+    return redirect_to articles_path, notice: 'ログイン' if logged_in?
     login_at(params[:provider])
-
-    puts params[:provider]
   end
 
   def callback
     provider = params[:provider]
-  
-    puts params[:provider]
-
     if @user = login_from(provider)
-      redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
-
-    puts params[:provider]
-
+      redirect_to articles_path, success: "#{provider.titleize}アカウントでログインしました。"
     else
       begin
         @user = create_from(provider)
         reset_session
         auto_login(@user)
-        redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
-
-        puts params[:provider]
+        redirect_to articles_path, success: "#{provider.titleize}アカウントでログインしました。"
       rescue
         redirect_to root_path, error: "#{provider.titleize}アカウントでのログインに失敗しました。"
       end
