5b2389db4e509b8c8de9d4a38f30cd52667f6951
s17w09 <s0917.smys04@gmail.com>
2024-07-24 14:54:43 +0900

    rubocop, eslint実施

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 OpenAI.configure do |config|
-    config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
-    config.log_errors = true # Highly recommended in development, so you can see what errors OpenAI is returning. Not recommended in production because it could leak private data to your logs.
-  end
+  config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+  config.log_errors = true # Highly recommended in development, so you can see what errors OpenAI is returning. Not recommended in production because it could leak private data to your logs.
+end
