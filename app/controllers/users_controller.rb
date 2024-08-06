0a5298f231b90ad7bfe60be281708259cb260c41
s17w09 <s0917.smys04@gmail.com>
2024-08-06 16:59:15 +0900

    update #122 チャットボット支援（書き終えた後のフォロー）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,34 +43,30 @@ class UsersController < ApplicationController
   end
 
   def my_dashboards
-    @article = current_user.articles.published
+    @article_published = current_user.articles.published
+    @article_favorite = current_user.favorites
+    @article_title = current_user.articles.published.pluck(:title)
+    @article_body = current_user.articles.published.pluck(:body).map { |body| body.truncate(200) }
 
-    def text
-      @user_input = params[:user_input]
-      return if @user_input.blank?
+    @user_input = params[:user_input]
+    return if @user_input.blank?
 
-      client = OpenAI::Client.new
+    client = OpenAI::Client.new
 
-      response = client.chat(
-        parameters: {
-          model: 'gpt-4o-mini', # Required.
-          messages: [
-            { role: 'system', content: "あなたは優秀なWEBエンジニアです。初心者エンジニアでもわかるように、userの質問に回答してください。
-        ユーザーから、「「」についてテンプレート化してください。」という文言とその一言が送信されたら、鉤括弧内の文言をもとに、初心者エンジニアでも書きやすい技術記事のテンプレートを、3~5項目で作成してください。
-        ユーザーから、「「」について内容を正確に確認し、誤っていれば内容を指摘してください。」という文言とともに記事内容が送信されたら、鉤括弧内の内容をもとに、内容の正確性を確認し誤っていれば内容を指摘してください。
-        ユーザーから、「「」について誤字脱字、文章構成を確認し、誤っていれば内容を指摘してください。」の内容とともに質問などが送られてきたら、鉤括弧内の内容をもとに、誤字脱字・文章構成を確認し誤っていれば内容を指摘してください。
-        ユーザーから、上記以外の内容が送信されたら、その内容に正確に回答してください。
-        " },
-            { role: 'user', content: @user_input }
-          ], # Required.
-          temperature: 0.7,
-          max_tokens: 600 # 平均でひらがな１文字１～２トークン、漢字１文字２～３トークンのため、日本語300文字程度と考え600に設定
-        }
-      )
+    response = client.chat(
+      parameters: {
+        model: 'gpt-4o-mini', # 必要なモデルを指定
+        messages: [
+          { role: 'system', content: 'あなたは優秀なWEBエンジニアです。ユーザー入力に応答してください。' },
+          { role: 'user', content: @user_input }
+        ],
+        temperature: 0.7,
+        max_tokens: 600 # 平均でひらがな１文字１～２トークン、漢字１文字２～３トークンのため、日本語300文字程度と考え600に設定
+      }
+    )
 
-      @text = response.dig('choices', 0, 'message', 'content')
-      render json: { text: @text }
-    end
+    @text = response.dig('choices', 0, 'message', 'content')
+    render json: { text: @text }
   end
 
   private
