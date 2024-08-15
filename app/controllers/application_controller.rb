2ebbc2a47ed5724ce06dee8596dbfaa1d80874e0
s17w09 <s0917.smys04@gmail.com>
2024-08-15 17:45:31 +0900

    fix #227 ログアウト時の各挙動確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   private
 
   def not_authenticated
-    redirect_to root_path
+    redirect_to login_path
   end
 end
--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class StaticpagesController < ApplicationController
-  skip_before_action :require_login, only: [:top]
+  skip_before_action :require_login
   def top; end
 end
