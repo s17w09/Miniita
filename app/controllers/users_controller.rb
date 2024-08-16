c967f944a627eddab42ec13d67a8079287f64dc0
s17w09 <s0917.smys04@gmail.com>
2024-08-16 23:02:51 +0900

    fix #122 チャットボット支援（書き終えた後のフォロー、タイトルレビュー）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,6 @@ class UsersController < ApplicationController
     @article_published = current_user.articles.published
     @article_favorite = current_user.favorites
     @article_title = current_user.articles.published.pluck(:title)
-    @article_body = current_user.articles.published.pluck(:body)
 
     @user_input = params[:user_input]
     return if @user_input.blank?
@@ -58,11 +57,11 @@ class UsersController < ApplicationController
       parameters: {
         model: 'gpt-4o-mini', # 必要なモデルを指定
         messages: [
-          { role: 'system', content: 'あなたは優秀なWEBエンジニアです。ユーザー入力に応答してください。' },
+          { role: 'system', content: 'あなたは優秀なWEBエンジニアです。ユーザー入力に応答してください。max_tokensを1000で設定しているので、tokenを1000以内で文章を収めてください。' },
           { role: 'user', content: @user_input }
         ],
         temperature: 0.7,
-        max_tokens: 600 # 平均でひらがな１文字１～２トークン、漢字１文字２～３トークンのため、日本語300文字程度と考え600に設定
+        max_tokens: 1000 # 平均でひらがな１文字１～２トークン、漢字１文字２～３トークンのため、日本語300文字程度と考え600に設定
       }
     )
 
