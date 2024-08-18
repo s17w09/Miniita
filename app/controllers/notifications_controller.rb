161e41216ef3b1702ec8cedc1806f03e9ae280e5
s17w09 <s0917.smys04@gmail.com>
2024-08-18 13:09:47 +0900

    fix #258 【UI】通知表示数

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@
 class NotificationsController < ApplicationController
   def index
     # current_userが受け取った（passive_notification）の通知
-    @notifications = current_user.passive_notifications.page(params[:page])
+    @notifications = current_user.passive_notifications.page(params[:page]).per(13)
     # notificationの中でまだしていない通知のみ取り出す
     @notifications.where(checked: false).find_each do |notification|
       notification.update(checked: true)
