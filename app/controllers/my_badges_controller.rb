d9c3f6af389791541000c5d1fdbe1732e0296e3c
s17w09 <s0917.smys04@gmail.com>
2024-09-15 15:25:58 +0900

    バッジ機能のリファクタリング

--- app/controllers/my_badges_controller.rb
+++ app/controllers/my_badges_controller.rb
@@ -1,10 +1,3 @@
 class MyBadgesController < ApplicationController
-  def index
-    @first_article = current_user.first_published
-    @third_article = current_user.third_published
-    @fifth_article = current_user.fifth_published
-    @tenth_article = current_user.tenth_published
-    @twentieth_article = current_user.twentieth_published
-    @thirtieth_article = current_user.thirtieth_published
-  end
+  def index; end
 end
